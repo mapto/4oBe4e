a3f74bce85e622e05776335c03081a852db779c3
Martin Ruskov <martin.ruskov@gmail.com>
2020-04-02 23:17:45 +0200

    Added the main logic of the game

---
+++ main.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+"""The main standalone application for github.com/mapto/4oBe4e"""
+
+players = 4
+
+status = {1:[0]*4, 2:[0]*4, 3:[0]*4, 4:[0]*4}
+
+def roll_dice():    
+    """Rolls a dice: randomly generate a value between 1 and 6. Use `import random`.
+    """
+    pass
+
+def ask_move(player):
+    """Asks player which of his four pieces they want to move. Returns the piece index between 0 and 3."""
+    pass
+
+def do_move(player, move):
+    """Check if the move is valid. If it is, perform it. Returns whether it is valid."""
+    # TODO: Implement
+    return True
+
+def check_endgame():
+    """Check if any of the players has ended the game."""
+    # TODO: Implement
+    return True
+
+def end_game(winner):
+    """Celebrate the winning player."""
+    print(f"Player {winner} has won!")
+
+def start():
+    """The main game loop"""
+    win = False
+    player = 0
+    while not win:
+        dice = roll_dice()
+        
+        valid = False
+        while not valid: 
+            move = ask_move(player)
+            valid = do_move(player, move)
+
+        win = check_endgame()
+        if not win and dice != 6:
+            player = (player + 1) % players
+    end_game(player)
+
+if __name__ == "__main__":
+    start()
