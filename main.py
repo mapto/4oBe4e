cd59f99c53eebce3f19c14224a1dc4a4fb0115ce
Martin Ruskov <martin.ruskov@gmail.com>
2020-04-03 09:07:30 +0200

    Draws the frame of the board to make it clearer what needs to be done

--- main.py
+++ main.py
@@ -9,9 +9,22 @@ status = {1:[0]*4, 2:[0]*4, 3:[0]*4, 4:[0]*4}
 
 def redraw_board():
     """Draws the board with the current pieces. Do with ASCII for now.
+    Return a 21x21 board, represented by 21 lines of 21 characters each.
     @vkantsev can do this
     """
-    pass
+    # TODO: Implement inside of board. As first step draw a blank board.
+    # TODO: Then we'll discuss how to add the pieces with their current positions 
+    result = "/" + "-" * 19 + "\\\n"
+    for i in range(0, 19):
+        result += "|" + " " * 19 + "|\n"
+    result += "\\" + "-" * 19 + "/\n"
+    return result
+
+def redraw(player):
+    """The screen update function. Do not modify this for now."""
+    print("\n\n\n")
+    print(redraw_board())
+    print()
 
 def roll_dice():    
     """Rolls a dice: randomly generate a value between 1 and 6. Use `import random`.
@@ -23,6 +36,8 @@ def ask_move(player):
     """Asks player which of his four pieces they want to move. Returns the piece index between 0 and 3.
     @lankata can do this
     """
+    print(f"It is player {player}'s turn.")
+    print()
     pass
 
 def do_move(player, move):
@@ -44,8 +59,8 @@ def start():
     """The main game loop"""
     win = False
     player = 0
+    redraw(player)
     while not win:
-        redraw_board()
         dice = roll_dice()
         
         valid = False
@@ -56,6 +71,7 @@ def start():
         win = check_endgame()
         if not win and dice != 6:
             player = (player + 1) % players
+        redraw(player)
     end_game(player)
 
 if __name__ == "__main__":
