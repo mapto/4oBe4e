4e8884695acabcb338d523ff370ce348b46450bb
Veselin Kantsev <veselin@kantsev.com>
2020-04-06 00:13:20 +0100

    feat(board): Add a more complete version of the board

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 """The main standalone application for github.com/mapto/4oBe4e"""
 
@@ -10,52 +10,99 @@ players = 4
 status = {1: [0] * 4, 2: [0] * 4, 3: [0] * 4, 4: [0] * 4}
 
 
-def redraw_board():
-    """ Draw an ASCII board with the current pieces. 
+def draw_board() -> str:
+    """ Draw an ASCII board with the current pieces.
     """
 
     # TODO (@vkantsev): Discuss how to add the pieces with their current positions
 
-    BOARD_WIDTH = 45  # width excluding border
-    POS_SHAPE = "( )"  # unit position shape
-    FILLER = " * "  # filler
-    BORDER_H = "-"  # horizontal
-    BORDER_V = "|"  # vertical
+    from colorama import Back, Fore, Style
 
-    # Header
-    board = "/" + BORDER_H * BOARD_WIDTH + "\\" + "\n"
+    ROWS = 19
+    COLS = 19
+    HOME_SHAPE = "[ ]"
+    TARGET_SHAPE = "{ }"
+    FOOTPATH_SHAPE = "( )"
+    FINISH_SHAPE = " @ "
 
-    # Top half
-    board += BORDER_V + (POS_SHAPE * 3).center(BOARD_WIDTH) + BORDER_V + "\n"
-    for i in range(5):
-        board += BORDER_V + "( ) * ( )".center(BOARD_WIDTH) + BORDER_V + "\n"
+    # Init board
+    board = tuple([[Style.RESET_ALL + "   "] * COLS for row in range(ROWS)])
 
-    # Middle
-    board += BORDER_V + POS_SHAPE * 7 + FILLER + POS_SHAPE * 7 + BORDER_V + "\n"
-    board += BORDER_V + POS_SHAPE + FILLER * 13 + POS_SHAPE + BORDER_V + "\n"
-    board += BORDER_V + POS_SHAPE * 7 + FILLER + POS_SHAPE * 7 + BORDER_V + "\n"
+    # Define players' board attributes
+    players = (
+        {
+            "colour": "RED",
+            "home": ((5, (2, 3)), (6, (2, 3))),
+            "target": ((7, (6, 7)), (8, (6, 7))),
+            "finish": ((9, [*range(3, 8)]), None),
+        },
+        {
+            "colour": "BLUE",
+            "home": ((2, (12, 13)), (3, (12, 13))),
+            "target": ((6, (10, 11)), (7, (10, 11))),
+            "finish": ((3, [9]), (4, [9]), (5, [9]), (6, [9]), (7, [9])),
+        },
+        {
+            "colour": "GREEN",
+            "home": ((12, (15, 16)), (13, (15, 16))),
+            "target": ((10, (11, 12)), (11, (11, 12))),
+            "finish": ((9, [*range(11, 16)]), None),
+        },
+        {
+            "colour": "YELLOW",
+            "home": ((15, (5, 6)), (16, (5, 6))),
+            "target": ((11, (7, 8)), (12, (7, 8))),
+            "finish": ((11, [9]), (12, [9]), (13, [9]), (14, [9]), (15, [9])),
+        },
+    )
 
-    # Lower half
-    for i in range(5):
-        board += (
-            BORDER_V
-            + (POS_SHAPE + FILLER + POS_SHAPE).center(BOARD_WIDTH)
-            + BORDER_V
-            + "\n"
-        )
-    board += BORDER_V + (POS_SHAPE * 3).center(BOARD_WIDTH) + BORDER_V + "\n"
+    # Fill board frame
+    for i in range(len(board)):
+        board[i][:: len(board[i]) - 1] = [Fore.MAGENTA + " . ", Fore.MAGENTA + " . "]
 
-    # Footer
-    board += "\\" + BORDER_H * BOARD_WIDTH + "/" + "\n"
+    for i in 0, -1:
+        board[i][:] = (Fore.CYAN + " . ") * len(board[i])
+
+    # Fill player areas
+    for p in players:
+        for h in p["home"]:
+            for c in h[1]:
+                board[h[0]][c] = eval(f"Fore.{p['colour']}") + HOME_SHAPE
+        for t in p["target"]:
+            for c in t[1]:
+                board[t[0]][c] = eval(f"Fore.{p['colour']}") + TARGET_SHAPE
+        for f in p["finish"]:
+            if f:
+                for c in f[1]:
+                    board[f[0]][c] = eval(f"Fore.{p['colour']}") + FINISH_SHAPE
+
+    # Fill footpath
+    footpath = (
+        ((2, 16), (8, 9, 10)),
+        ((3, 4, 14, 15), (8, 10)),
+        ((5, 13), (5, 6, 7, 8, 10, 11, 12, 13)),
+        ((6, 7, 11, 12), (5, 13)),
+        ((8, 10), (2, 3, 4, 5, 13, 14, 15, 16)),
+        ([9], (2, 16)),
+    )
+
+    for p in footpath:
+        for r in p[0]:
+            for c in p[1]:
+                board[r][c] = Fore.WHITE + FOOTPATH_SHAPE
+
+    # Fill Trophies
+    board[9][9] = ">|<"
+    board[-1][-1] = Style.RESET_ALL + "🏳️‍🌈"
 
     return board
 
 
-def redraw(player):
+def redraw(player, board):
     """The screen update function. Do not modify this for now."""
-    print("\n\n\n")
-    print(redraw_board())
-    print()
+
+    for row in board:
+        print("".join(row))
 
 
 def end_game(winner):
@@ -63,11 +110,11 @@ def end_game(winner):
     print("Player {} has won!".format(winner))
 
 
-def start(players):
+def start(players, board):
     """The main game loop"""
     win = False
     player = choose_first(players)
-    redraw(player)
+    redraw(player, board)
     while not win:
         dice = roll_dice(player)
 
@@ -79,9 +126,9 @@ def start(players):
         win = check_endgame()
         if not win and dice != 6:
             player = ((player + 1) % players) + 1
-        redraw(player)
+        redraw(player, board)
     end_game(player)
 
 
 if __name__ == "__main__":
-    start(players)
+    start(players, draw_board())
--- main_test.py
+++ main_test.py
@@ -1,9 +1,11 @@
-def test_redraw_board():
+def test_draw_board():
     """ Test against a base64 representation of the ASCII board. """
 
-    from main import redraw_board
-    from base64 import b64encode
+    from main import draw_board
+    from hashlib import md5
 
-    board_b64 = b"Ly0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLVwKfCAgICAgICAgICAgICAgICAgICggKSggKSggKSAgICAgICAgICAgICAgICAgIHwKfCAgICAgICAgICAgICAgICAgICggKSAqICggKSAgICAgICAgICAgICAgICAgIHwKfCAgICAgICAgICAgICAgICAgICggKSAqICggKSAgICAgICAgICAgICAgICAgIHwKfCAgICAgICAgICAgICAgICAgICggKSAqICggKSAgICAgICAgICAgICAgICAgIHwKfCAgICAgICAgICAgICAgICAgICggKSAqICggKSAgICAgICAgICAgICAgICAgIHwKfCAgICAgICAgICAgICAgICAgICggKSAqICggKSAgICAgICAgICAgICAgICAgIHwKfCggKSggKSggKSggKSggKSggKSggKSAqICggKSggKSggKSggKSggKSggKSggKXwKfCggKSAqICAqICAqICAqICAqICAqICAqICAqICAqICAqICAqICAqICAqICggKXwKfCggKSggKSggKSggKSggKSggKSggKSAqICggKSggKSggKSggKSggKSggKSggKXwKfCAgICAgICAgICAgICAgICAgICggKSAqICggKSAgICAgICAgICAgICAgICAgIHwKfCAgICAgICAgICAgICAgICAgICggKSAqICggKSAgICAgICAgICAgICAgICAgIHwKfCAgICAgICAgICAgICAgICAgICggKSAqICggKSAgICAgICAgICAgICAgICAgIHwKfCAgICAgICAgICAgICAgICAgICggKSAqICggKSAgICAgICAgICAgICAgICAgIHwKfCAgICAgICAgICAgICAgICAgICggKSAqICggKSAgICAgICAgICAgICAgICAgIHwKfCAgICAgICAgICAgICAgICAgICggKSggKSggKSAgICAgICAgICAgICAgICAgIHwKXC0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS8K"
+    board_md5 = "e0126f60aff057284b112d0e19286dc0"
 
-    assert b64encode(redraw_board().encode("utf-8")) == board_b64
+    board = str(draw_board()).encode("utf-8")
+
+    assert md5(board).hexdigest() == board_md5
