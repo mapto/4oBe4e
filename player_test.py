da4099a65277be2abdf811157274a4892d31a343
Veselin Kantsev <veselin@kantsev.com>
2020-04-08 18:22:12 +0100

    feat(test): Add player.py unit tests #2

--- player_test.py
+++ player_test.py
@@ -10,7 +10,6 @@ import player
 def test_ask_move_invalid_input(monkeypatch):
     for test_input in ["a_string", "", " ", "\n", -1, 1.3, 4, None, False]:
         pawn_number = StringIO(f"{test_input}\n")
-
         monkeypatch.setattr("sys.stdin", pawn_number)
 
         with pytest.raises(Exception):
@@ -20,7 +19,6 @@ def test_ask_move_invalid_input(monkeypatch):
 def test_ask_move_valid_input(monkeypatch):
     for test_input in [0, 1, 2, 3]:
         pawn_number = StringIO(f"{test_input}\n")
-
         monkeypatch.setattr("sys.stdin", pawn_number)
 
         assert player.ask_move(1) == test_input
