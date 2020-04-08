315f925ca0fec7c480bad03e78271caec99d86c2
Veselin Kantsev <veselin@kantsev.com>
2020-04-08 19:21:40 +0100

    fix(test): Remove loop as masks intermediate exception instances

--- player_test.py
+++ player_test.py
@@ -9,17 +9,47 @@ import player
 
 
 def test_ask_move_invalid_input(monkeypatch):
-    for test_input in ["a_string", "", " ", "\n", -1, 1.3, 4, None, False]:
-        pawn_number = StringIO(f"{test_input}\n")
-        monkeypatch.setattr("sys.stdin", pawn_number)
 
-        with pytest.raises(Exception):
-            assert player.ask_move(1)
+    pawn_number = StringIO(f"\n")
+    monkeypatch.setattr("sys.stdin", pawn_number)
+    with pytest.raises(Exception):
+        assert player.ask_move(1)
+
+    pawn_number = StringIO("a_string\n")
+    monkeypatch.setattr("sys.stdin", pawn_number)
+    with pytest.raises(Exception):
+        assert player.ask_move(1)
+
+    pawn_number = StringIO(f"-1\n")
+    monkeypatch.setattr("sys.stdin", pawn_number)
+    with pytest.raises(Exception):
+        assert player.ask_move(1)
+
+    pawn_number = StringIO(f"1.3\n")
+    monkeypatch.setattr("sys.stdin", pawn_number)
+    with pytest.raises(Exception):
+        assert player.ask_move(1)
+
+    pawn_number = StringIO(f"4\n")
+    monkeypatch.setattr("sys.stdin", pawn_number)
+    with pytest.raises(Exception):
+        assert player.ask_move(1)
 
 
 def test_ask_move_valid_input(monkeypatch):
-    for test_input in [0, 1, 2, 3]:
-        pawn_number = StringIO(f"{test_input}\n")
-        monkeypatch.setattr("sys.stdin", pawn_number)
 
-        assert player.ask_move(1) == test_input
+    pawn_number = StringIO("0\n")
+    monkeypatch.setattr("sys.stdin", pawn_number)
+    assert player.ask_move(1) == 0
+
+    pawn_number = StringIO("1\n")
+    monkeypatch.setattr("sys.stdin", pawn_number)
+    assert player.ask_move(1) == 1
+
+    pawn_number = StringIO("2\n")
+    monkeypatch.setattr("sys.stdin", pawn_number)
+    assert player.ask_move(1) == 2
+
+    pawn_number = StringIO("3\n")
+    monkeypatch.setattr("sys.stdin", pawn_number)
+    assert player.ask_move(1) == 3
