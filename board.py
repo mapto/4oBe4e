05d3d0dba5198d9d03df0a0728c80fbacfabd0eb
Lucho <luchesar@gmail.com>
2020-04-09 08:14:23 +0100

    Proposition for modelling the game play model: build fix

--- board.py
+++ board.py
@@ -1,15 +1,25 @@
+from piece import Piece
+from typing import Any, List, Tuple, Dict
+
+
 class Board:
-  def __init__(self, number_layers: int = 4, numberOfPieces: int = 4):
-    self.numberPlayers = numberOf
-    self.numberOfPieces = numberPlayers
-    self.pieces: List[Pieces] = [new Piece()]
-    self.winners: List[int] = []
+    def __init__(self, number_of_players: int = 4, number_of_pieces: int = 4):
+        self.number_of_players = number_of_players
+        self.number_of_pieces = number_of_pieces
+        # self.pieces: List[Piece] = [] # TODO generate the initial state of the pieces
+        # self.winners: List[int] = []
 
-  def moveForward(p: Piece, positions: int) -> List[int]:
+    def moveForward(self, piece: Piece, positions: int) -> List[int]:
+        """
+          return the winners after 
+        """
+        return []
 
-  def winners() -> List[int]:
-    return self.winners
+    def winners(self) -> List[int]:
+        return []
 
-  def pieces() -> List[Piece]:
-    return self.peices
-  
+    def pieces(self) -> List[Piece]:
+        return []
+
+    def knocks_out(self, moving: Piece, static: Piece) -> bool:
+        return True
--- piece.py
+++ piece.py
@@ -1,8 +1,16 @@
-class Piece:
-  def __init__(self, pience_number: int, player: int, absolutePosition: int):
-    self.piece_number: int = pience_number
-    self.player: int = player
-    self.absolutePosition: int = absolutePosition
-    self.position: int = 0
+from typing import Any
 
-  def __eq__(self, other: Any) -> bool:  
+
+class Piece:
+    def __init__(self, pience_number: int, player: int, absolutePosition: int):
+        self.piece_number: int = pience_number
+        self.player: int = player
+        self.absolutePosition: int = absolutePosition
+        self.position: int = 0
+
+    def __eq__(self, other: Any) -> bool:
+        return (
+            self.__class__ == other.__class__
+            and self.piece_number == other.piece_number
+            and self.player == other.player
+        )
