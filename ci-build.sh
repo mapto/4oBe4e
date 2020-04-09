852bcb3cff2810b90830f906e843d315310315e7
Martin Ruskov <martin.ruskov@gmail.com>
2020-04-09 21:12:40 +0200

    Extracted code in src

--- ci-build.sh
+++ ci-build.sh
@@ -3,13 +3,13 @@
 export PIPENV_VERBOSITY=-1
 
 echo '>>> Run Black'
-pipenv run black .
+pipenv run black src
 
 echo '>>> Run Pylint'
-pipenv run pylint -E *.py
+pipenv run pylint -E src/*.py
 
 echo '>>> Run Mypy'
-pipenv run mypy .
+pipenv run mypy src
 
 echo '>>>  Run Pytest'
-pipenv run pytest -v --doctest-modules .
+pipenv run pytest -v --doctest-modules src
