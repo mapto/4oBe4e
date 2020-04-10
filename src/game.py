511d872fab4853eee4fd12e6b882f9b1a44e317c
Veselin Kantsev <veselin@kantsev.com>
2020-04-10 19:22:29 +0100

    feat(game): Add coord_on_path func (#8)
    * feat(game): Add coord_on_path implementation
    
    * add final func
    
    * fix(game): Fix assert callable
    
    * test(game): Enable doctests

--- src/game.py
+++ src/game.py
@@ -88,34 +88,112 @@ def coord_on_path(piece: Piece) -> Tuple[int, int]:
     draw a placeholder, which does not need to show piece number
     possibly split this in 4 or 8 different cases.
     Parameter piece does't influence logic
-    
-    >>> coord_on_path(Piece(1, 1, 1))
-    (2, 8)
 
-    The following tests currently fail, thus disabled when pushed. Enable for development:
+    Player (absolute) Progress to (relative) Position conversion:
+        P0     1..56: (pos)
+        P1     1..42: (p_num * shift + pos)
+              43..56: (p_num * shift + pos) % end_progress
+        P2     1..28: (p_num * shift + pos)
+              29..56: (p_num * shift + pos) % end_progress
+        P3     1..14: (p_num * shift + pos)
+              15..56: (p_num * shift + pos) % end_progress
+
+
+    Test player 1:
+    >>> coord_on_path(Piece(0, 1, 1))
+    (8, 2)
 
     Test player 2:
-    >> coord_on_finish(Piece(2, 1, 2))
-    (10, 3)
+    >>> coord_on_path(Piece(1, 1, 1))
+    (2, 10)
 
     Test player 3:
-    >> coord_on_finish(Piece(3, 1, 3))
-    (14, 10)
+    >>> coord_on_path(Piece(2, 1, 1))
+    (10, 16)
+
+    Test player 4:
+    >>> coord_on_path(Piece(3, 1, 1))
+    (16, 8)
+
+    Test path wrap:
+    >>> coord_on_path(Piece(3, 1, 56))
+    (16, 9)
 
     Test overlap:
-    >> coord_on_finish(Piece(2, 1, 17))
-    (14, 10)
-
-    Test turn:
-    >> coord_on_finish(Piece(3, 1, 5))
-    (13, 11)
-
-    Test last position:
-    >> coord_on_finish(Piece(4, 1, 56))
-    (9, 16)
+    >> coord_on_path(Piece(2, 1, 17))
+    (10, 14)
     """
 
-    return (2, 8)
+    assert 1 <= piece.progress() <= 56 and 0 <= piece.player() <= 3
+
+    POSITION_TO_ROWCOL: Tuple[Tuple[int, int], ...] = (
+        (0, 0),
+        (8, 2),
+        (8, 3),
+        (8, 4),
+        (8, 5),
+        (7, 5),
+        (6, 5),
+        (5, 5),
+        (5, 6),
+        (5, 7),
+        (5, 8),
+        (4, 8),
+        (3, 8),
+        (2, 8),
+        (2, 9),
+        (2, 10),
+        (3, 10),
+        (4, 10),
+        (5, 10),
+        (5, 11),
+        (5, 12),
+        (5, 13),
+        (6, 13),
+        (7, 13),
+        (8, 13),
+        (8, 14),
+        (8, 15),
+        (8, 16),
+        (9, 16),
+        (10, 16),
+        (10, 15),
+        (10, 14),
+        (10, 13),
+        (11, 13),
+        (12, 13),
+        (13, 13),
+        (13, 12),
+        (13, 11),
+        (13, 10),
+        (14, 10),
+        (15, 10),
+        (16, 10),
+        (16, 9),
+        (16, 8),
+        (15, 8),
+        (14, 8),
+        (13, 8),
+        (13, 7),
+        (13, 6),
+        (13, 5),
+        (12, 5),
+        (11, 5),
+        (10, 5),
+        (10, 4),
+        (10, 3),
+        (10, 2),
+        (9, 2),
+    )
+
+    progress = piece.progress()
+    player = piece.player()
+    shift = 14
+    path_wrap = 56
+    position = player * shift + progress
+    position = position if position <= path_wrap else position % path_wrap
+
+    return POSITION_TO_ROWCOL[position]
 
 
 def coord_on_finish(piece: Piece) -> Tuple[int, int]:
