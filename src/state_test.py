b7db33b09682042d7bd33fca490ea2663f5da5d6
Lucho <luchesar@gmail.com>
2020-04-14 21:58:29 +0100

    Better handling of the game players numbers

--- src/state_test.py
+++ src/state_test.py
@@ -4,7 +4,7 @@ from state import Board, Piece, GameState, RollDice
 
 def test_default_board_init(monkeypatch):
     board = Board.create()
-    assert board.players_count == 4
+    assert board.players == [0, 1, 2, 3]
     assert board.pieces_per_player == 4
     assert board.shape_angles == 4
     assert board.shape_side_length == 14
@@ -42,8 +42,8 @@ def test_2_players_board_init(monkeypatch):
     """Make sure if we have just two players in a 4 angle board for them
       to be at the opposite corners instead of next to each other.
     """
-    board = Board.create(players_count=2)
-    assert board.players_count == 2
+    board = Board.create(players=[1, 3])
+    assert board.players == [1, 3]
     assert board.pieces_per_player == 4
     assert board.shape_angles == 4
     assert board.shape_side_length == 14
@@ -67,8 +67,8 @@ def test_3_players_5_angle_board_init(monkeypatch):
     """Make sure if we have just 3 players in a 5 angle board for them
       to be at the opposite corners instead of next to each other.
     """
-    board = Board.create(players_count=3, shape_angles=5)
-    assert board.players_count == 3
+    board = Board.create(players=[0, 2, 3], shape_angles=5)
+    assert board.players == [0, 2, 3]
     assert board.pieces_per_player == 4
     assert board.shape_angles == 5
     assert board.shape_side_length == 14
@@ -92,15 +92,15 @@ def test_3_players_5_angle_board_init(monkeypatch):
     ]
 
 
-def test_sutom_board_init(monkeypatch):
+def test_custom_board_init(monkeypatch):
     board = Board.create(
-        players_count=2,
+        players=[0, 1],
         pieces_per_player=1,
         shape_angles=5,
         shape_side_length=13,
         finish_zone_length=3,
     )
-    assert board.players_count == 2
+    assert board.players == [0, 1]
     assert board.pieces_per_player == 1
     assert board.shape_angles == 5
     assert board.shape_side_length == 13
@@ -112,7 +112,7 @@ def test_sutom_board_init(monkeypatch):
 
 def test_game_state_defaults(monkeypatch):
     board = Board.create()
-    state = GameState(board)
+    state = GameState.create(board)
 
     assert state.board == board
     assert state.number == 0
