8fed97f7887b1183a070e6302b2badbe007fda37
Lucho <luchesar@gmail.com>
2020-04-15 20:06:30 +0100

    Making the tests pass

--- ci-build.sh
+++ ci-build.sh
@@ -6,9 +6,6 @@ export PIPENV_VERBOSITY=-1
 
 cd $(git rev-parse --show-toplevel)
 
-echo '>>> Run Black'
-pipenv run black src
-
 echo '>>> Run Pylint'
 pipenv run pylint -E src/
 
@@ -17,3 +14,7 @@ pipenv run mypy src
 
 echo '>>>  Run Pytest'
 pipenv run pytest -v --doctest-modules src
+
+echo '>>> Run Black'
+pipenv run black src
+
