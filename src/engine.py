2f7f13b43d5306ceccba192866e98c08674fff6b
Lucho <luchesar@gmail.com>
2020-04-15 20:06:30 +0100

    Making the tests pass

--- src/engine.py
+++ src/engine.py
@@ -1,17 +1,37 @@
 from state import Piece, Board, GameAction, RollDice, MovePiece, PieceOut, GameState
+from util import roll as roll_dice
+from typing import List, Sequence
 
-# from pampy import match, _
+
+class Dice:
+    def roll(self) -> int:
+        return roll_dice()
 
 
 class GameEngine:
-    def __init__(self, board: Board):
+    def __init__(self, board: Board, dice: Dice = Dice()):
         self.state = GameState.create(board)
+        self.dice = dice
 
     def get_state(self) -> GameState:
         return self.state
 
     def on_roll_dice(self, player: int) -> GameState:
-        pass
+        dice = self.dice.roll()
+        self.state.dice = dice
+        player = self.state.current_player
+        valid_actions: List[GameAction] = []
+
+        def calc_valid_actions(piece: Piece) -> None:
+            if piece.position == 0 and dice == 6:
+                valid_actions.append(PieceOut(player, piece, dice))
+
+        for piece in self.state.board.pieces:
+            if piece.player == player:
+                calc_valid_actions(piece)
+
+        self.state.valid_actions = valid_actions
+        return self.state
 
     def on_piece_out(self, player: int, piece: Piece, dice: int) -> GameState:
         pass
