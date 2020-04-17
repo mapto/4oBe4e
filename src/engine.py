b18eee2ef8c7dec289c22c243218ca0471b4fb9f
Luchesar Cekov <luchesar@gmail.com>
2020-04-17 19:32:28 +0100

    Define state of the game and the engine class (#9)
    * Define state of the game and the engine class
    
    * some more tests for the game state
    
    * Few more touches on the game engine
    
    * Better handling of the game players numbers
    
    * Board.__post_init__ validations
    
    * Set build to python 3.6 which is the latest available in most distros
    
    * Making the tests pass
    
    * add pipenv webapp notes (#10)
    
    * Set build to python 3.6 which is the latest available in most distros
    
    * Implement some more of the gameplay
    
    * add pipenv webapp notes
    
    * style #2
    
    * Fixed dummy-webapp and flask serving static files
    
    * Fixed piece positioning that was broken due to wrongly 1-indexed players
    
    * Improved piece visualisation: colours and overlaps
    
    * <update on html and css files>
    
    * Define state of the game and the engine class
    
    * some more tests for the game state
    
    * Few more touches on the game engine
    
    * Better handling of the game players numbers
    
    * Board.__post_init__ validations
    
    * Making the tests pass
    
    * Implement some more of the gameplay
    
    * Define state of the game and the engine class
    
    * some more tests for the game state
    
    * Few more touches on the game engine
    
    * Better handling of the game players numbers
    
    * Board.__post_init__ validations
    
    * Making the tests pass
    
    * Implement some more of the gameplay
    
    * Define state of the game and the engine class
    
    * some more tests for the game state
    
    * format
    
    * fix build for pr merge
    
    Co-authored-by: Veselin Kantsev <4179342+vkantsev@users.noreply.github.com>
    Co-authored-by: Martin Ruskov <martin.ruskov@gmail.com>
    Co-authored-by: Ilian Gachev <ilian_gachev@abv.bg>

---
+++ src/engine.py
@@ -0,0 +1,53 @@
+from state import Piece, Board, GameAction, RollDice, MovePiece, PieceOut, GameState
+from util import roll as roll_dice
+from typing import List, Sequence
+
+
+class Dice:
+    def roll(self) -> int:
+        return roll_dice()
+
+
+class GameEngine:
+    def __init__(self, board: Board, dice: Dice = Dice()):
+        self.state = GameState.create(board)
+        self.dice = dice
+
+    def get_state(self) -> GameState:
+        return self.state
+
+    def __on_roll_dice(self, player: int) -> GameState:
+        dice = self.dice.roll()
+        self.state.dice = dice
+        player = self.state.current_player
+        valid_actions: List[GameAction] = []
+
+        def calc_valid_actions(piece: Piece) -> None:
+            if piece.position == 0 and dice == 6:
+                valid_actions.append(PieceOut(player, piece, dice))
+
+        for piece in self.state.board.pieces:
+            if piece.player == player:
+                calc_valid_actions(piece)
+        if len(valid_actions) == 0:
+            valid_actions.append(RollDice(player))
+
+        self.state.valid_actions = valid_actions
+        return self.state
+
+    def __on_piece_out(self, player: int, piece: Piece, dice: int) -> GameState:
+        pass
+
+    def __on_move_piece(self, player: int, piece: Piece, dice: int) -> GameState:
+        pass
+
+    def play(self, action: GameAction) -> GameState:
+        if action not in self.state.valid_actions:
+            raise ValueError(
+                f"Illegal action {action} is not one of {self.state.valid_actions}"
+            )
+
+        if isinstance(action, RollDice):
+            self.__on_roll_dice(action.player)
+
+        return self.state
---
+++ src/state_test.py
@@ -0,0 +1,154 @@
+import pytest  # type: ignore
+from state import Board, Piece, GameState, RollDice
+
+
+def test_default_board_init(monkeypatch):
+    board = Board.create()
+    assert board.players == [0, 1, 2, 3]
+    assert board.pieces_per_player == 4
+    assert board.shape_angles == 4
+    assert board.shape_side_length == 14
+    assert board.player_shift == 15
+    assert board.finish_zone_length == 5
+    assert board.end_progress == 62
+    assert len(board.pieces) == 16
+    assert list(filter(lambda p: p.player == 0, board.pieces)) == [
+        Piece(0, 0, 0),
+        Piece(1, 0, 0),
+        Piece(2, 0, 0),
+        Piece(3, 0, 0),
+    ]
+    assert list(filter(lambda p: p.player == 1, board.pieces)) == [
+        Piece(0, 1, 0),
+        Piece(1, 1, 0),
+        Piece(2, 1, 0),
+        Piece(3, 1, 0),
+    ]
+    assert list(filter(lambda p: p.player == 2, board.pieces)) == [
+        Piece(0, 2, 0),
+        Piece(1, 2, 0),
+        Piece(2, 2, 0),
+        Piece(3, 2, 0),
+    ]
+    assert list(filter(lambda p: p.player == 3, board.pieces)) == [
+        Piece(0, 3, 0),
+        Piece(1, 3, 0),
+        Piece(2, 3, 0),
+        Piece(3, 3, 0),
+    ]
+
+
+def test_2_players_board_init(monkeypatch):
+    """Make sure if we have just two players in a 4 angle board for them
+      to be at the opposite corners instead of next to each other.
+    """
+    board = Board.create(players=[1, 3])
+    assert board.players == [1, 3]
+    assert board.pieces_per_player == 4
+    assert board.shape_angles == 4
+    assert board.shape_side_length == 14
+    assert board.player_shift == 15
+    assert board.finish_zone_length == 5
+    assert board.end_progress == 62
+    assert len(board.pieces) == 8
+    assert board.pieces == [
+        Piece(0, 0, 0),
+        Piece(1, 0, 0),
+        Piece(2, 0, 0),
+        Piece(3, 0, 0),
+        Piece(0, 1, 0),
+        Piece(1, 1, 0),
+        Piece(2, 1, 0),
+        Piece(3, 1, 0),
+    ]
+
+
+def test_3_players_5_angle_board_init(monkeypatch):
+    """Make sure if we have just 3 players in a 5 angle board for them
+      to be at the opposite corners instead of next to each other.
+    """
+    board = Board.create(players=[0, 2, 3], shape_angles=5)
+    assert board.players == [0, 2, 3]
+    assert board.pieces_per_player == 4
+    assert board.shape_angles == 5
+    assert board.shape_side_length == 14
+    assert board.player_shift == 15
+    assert board.finish_zone_length == 5
+    assert board.end_progress == 5 * 14 + 1 + 5
+    assert len(board.pieces) == 3 * 4
+    assert board.pieces == [
+        Piece(0, 0, 0),
+        Piece(1, 0, 0),
+        Piece(2, 0, 0),
+        Piece(3, 0, 0),
+        Piece(0, 1, 0),
+        Piece(1, 1, 0),
+        Piece(2, 1, 0),
+        Piece(3, 1, 0),
+        Piece(0, 2, 0),
+        Piece(1, 2, 0),
+        Piece(2, 2, 0),
+        Piece(3, 2, 0),
+    ]
+
+
+def test_custom_board_init(monkeypatch):
+    board = Board.create(
+        players=[0, 1],
+        pieces_per_player=1,
+        shape_angles=5,
+        shape_side_length=13,
+        finish_zone_length=3,
+    )
+    assert board.players == [0, 1]
+    assert board.pieces_per_player == 1
+    assert board.shape_angles == 5
+    assert board.shape_side_length == 13
+    assert board.player_shift == 14
+    assert board.finish_zone_length == 3
+    assert board.end_progress == 13 * 5 + 1 + 3
+    assert board.pieces == [Piece(0, 0, 0), Piece(0, 1, 0)]
+
+
+def test_negative_create_wrong_players_board(monkeypatch):
+    # player index bigger then the board
+    # with pytest.raises(Exception):
+    #    board = Board.create(players=[6, 1], shape_angles=5)
+    # board with no players
+    with pytest.raises(Exception):
+        board = Board.create(players=[])
+    # board with duplicate players
+    with pytest.raises(Exception):
+        board = Board.create(players=[1, 1])
+    # board with too many players
+    with pytest.raises(Exception):
+        board = Board.create(players=[0, 1, 2], shape_angles=2)
+
+
+def test_state_next_player(monkeypatch):
+    board = Board.create(players=[0, 1, 3, 5])
+    state = GameState.create(board)
+
+    assert state.current_player == 0
+    # assert state.next_player() == 1
+
+    state.current_player = 1
+    # assert state.next_player() == 3
+
+    state.current_player = 3
+    # assert state.next_player() == 5
+
+    state.current_player = 5
+    # assert state.next_player() == 0
+
+
+def test_game_state_defaults(monkeypatch):
+    board = Board.create()
+    state = GameState.create(board)
+
+    assert state.board == board
+    assert state.number == 0
+    assert state.dice == -1
+    assert state.winners == []
+    assert state.current_player == 0
+    assert state.valid_actions == [RollDice(player=0)]
