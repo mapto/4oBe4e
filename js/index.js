5417e8b3f4bfb453e56266ec2e29b875d5ac4e1b
Ilian Gachev <ilian_gachev@abv.bg>
2020-04-21 14:09:10 +0300

    new roll dice button and new name butonns

---
+++ js/index.js
@@ -0,0 +1,24 @@
+
+function rollDice() {
+    var randomNumber = Math.floor(Math.random() * 6) + 1; // 1 - 6
+    var playerOneImages = "images/red" +randomNumber+ ".png";
+
+    document.querySelector('.img1').setAttribute('src', playerOneImages)
+}
+
+function myFunction1() {
+    var x = document.getElementById("player1").value;
+    document.getElementById("demo1").innerHTML = x;
+  }
+function myFunction2() {
+    var x = document.getElementById("player2").value;
+    document.getElementById("demo2").innerHTML = x;
+  }
+function myFunction3() {
+    var x = document.getElementById("player3").value;
+    document.getElementById("demo3").innerHTML = x;
+  }
+  function myFunction4() {
+    var x = document.getElementById("player4").value;
+    document.getElementById("demo4").innerHTML = x;
+  }
