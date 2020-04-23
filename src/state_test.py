6d528cc26589064acd95376cfd056799845c8485
Veselin Kantsev <4179342+vkantsev@users.noreply.github.com>
2020-04-23 21:57:26 +0100

    return to 14 point sides

--- src/state_test.py
+++ src/state_test.py
@@ -7,11 +7,11 @@ def test_default_board_init(monkeypatch):
     board = Board.create()
     assert board.players == [0, 1, 2, 3]
     assert board.pieces_per_player == 4
-    assert board.board_corners == 4
+    assert board.board_sides == 4
     assert board.board_side_length == 14
-    assert board.player_shift == 15
+    assert board.player_shift == 14
     assert board.finish_zone_length == 5
-    assert board.end_progress == 66
+    assert board.end_progress == 62
     assert len(board.pieces) == 16
     assert list(filter(lambda p: p.player == 0, board.pieces)) == [
         Piece(0, 0, 0),
@@ -46,11 +46,11 @@ def test_2_players_board_init(monkeypatch):
     board = Board.create(players=[1, 3])
     assert board.players == [1, 3]
     assert board.pieces_per_player == 4
-    assert board.board_corners == 4
+    assert board.board_sides == 4
     assert board.board_side_length == 14
-    assert board.player_shift == 15
+    assert board.player_shift == 14
     assert board.finish_zone_length == 5
-    assert board.end_progress == 66
+    assert board.end_progress == 62
 
     assert len(board.pieces) == 8
     assert board.pieces == [
@@ -69,17 +69,17 @@ def test_3_players_5_corner_board_init(monkeypatch):
     """Make sure if we have just 3 players in a 5 corner board for them
       to be at the opposite corners instead of next to each other.
     """
-    board = Board.create(players=[0, 2, 3], board_corners=5)
+    board = Board.create(players=[0, 2, 3], board_sides=5)
     assert board.players == [0, 2, 3]
     assert board.pieces_per_player == 4
-    assert board.board_corners == 5
+    assert board.board_sides == 5
     assert board.board_side_length == 14
-    assert board.player_shift == 15
-    assert board.path_zone_length == 5 * 14 + 5
+    assert board.player_shift == 14
+    assert board.path_zone_length == 5 * 14
     assert board.finish_zone_length == 5
     # end_progress == path_zone_length + finish_zone_length + 1 THAT IS
-    # end_progress == ((board_corners * board_side_length) + board_corners) + finish_zone_length + 1
-    assert board.end_progress == ((5 * 14) + 5) + 5 + 1
+    # end_progress == (board_sides * board_side_length) + finish_zone_length + 1
+    assert board.end_progress == (5 * 14) + 5 + 1
     assert len(board.pieces) == 3 * 4
     assert board.pieces == [
         Piece(0, 0, 0),
@@ -101,25 +101,25 @@ def test_custom_board_init(monkeypatch):
     board = Board.create(
         players=[0, 1],
         pieces_per_player=1,
-        board_corners=5,
+        board_sides=5,
         board_side_length=13,
         finish_zone_length=3,
     )
     assert board.players == [0, 1]
     assert board.pieces_per_player == 1
-    assert board.board_corners == 5
+    assert board.board_sides == 5
     assert board.board_side_length == 13
-    assert board.player_shift == 14
-    assert board.path_zone_length == 5 * 13 + 5
+    assert board.player_shift == 13
+    assert board.path_zone_length == 5 * 13
     assert board.finish_zone_length == 3
-    assert board.end_progress == 5 * 13 + 5 + 3 + 1
+    assert board.end_progress == 5 * 13 + 3 + 1
     assert board.pieces == [Piece(0, 0, 0), Piece(0, 1, 0)]
 
 
 def test_negative_create_wrong_players_board(monkeypatch):
     # player index bigger then the board
     # with pytest.raises(Exception):
-    #    board = Board.create(players=[6, 1], board_corners=5)
+    #    board = Board.create(players=[6, 1], board_sides=5)
     # board with no players
     with pytest.raises(Exception):
         Board.create(players=[])
@@ -128,7 +128,7 @@ def test_negative_create_wrong_players_board(monkeypatch):
         Board.create(players=[1, 1])
     # board with too many players
     with pytest.raises(Exception):
-        Board.create(players=[0, 1, 2], board_corners=2)
+        Board.create(players=[0, 1, 2], board_sides=2)
 
 
 def test_state_next_player(monkeypatch):
@@ -178,13 +178,13 @@ def test_board_relative_position():
     assert rel_pos_p0 == 20
 
     rel_pos_p1 = board.relative_position(piece=Piece(number=0, player=1, position=20))
-    assert rel_pos_p1 == 35
+    assert rel_pos_p1 == 34
 
     rel_pos_p2 = board.relative_position(piece=Piece(number=0, player=2, position=20))
-    assert rel_pos_p2 == 50
+    assert rel_pos_p2 == 48
 
     rel_pos_p3 = board.relative_position(piece=Piece(number=0, player=3, position=20))
-    assert rel_pos_p3 == 5
+    assert rel_pos_p3 == 6
 
     # Test a position outside of path_zone
     with pytest.raises(Exception):
@@ -223,21 +223,24 @@ def test_board_is_on_path():
 def test_board_is_on_finish():
     board = Board.create()
 
-    p0_on_finish = board.is_on_finish(piece=Piece(number=0, player=0, position=60))
+    p0_on_finish = board.is_on_finish(piece=Piece(number=0, player=0, position=56))
     assert not p0_on_finish
 
     p0_on_finish = board.is_on_finish(piece=Piece(number=0, player=0, position=61))
     assert p0_on_finish
 
-    p0_on_finish = board.is_on_finish(piece=Piece(number=0, player=0, position=66))
+    p0_on_finish = board.is_on_finish(piece=Piece(number=0, player=0, position=62))
     assert not p0_on_finish
 
 
 def test_board_is_on_target():
     board = Board.create()
 
-    p0_on_target = board.is_on_target(piece=Piece(number=0, player=0, position=60))
+    p0_on_target = board.is_on_target(piece=Piece(number=0, player=0, position=61))
     assert not p0_on_target
 
-    p0_on_target = board.is_on_target(piece=Piece(number=0, player=0, position=66))
+    p0_on_target = board.is_on_target(piece=Piece(number=0, player=0, position=62))
     assert p0_on_target
+
+    p0_on_target = board.is_on_target(piece=Piece(number=0, player=0, position=66))
+    assert not p0_on_target
